c6e1a4b2a9406e30813f6fb098cba864dd971449
Enric Besora Navarro <enricbesora@gmail.com>
2024-11-14 10:33:21 +0100

    Spring Web correcto

--- MyFirstApplication/src/test/java/com/example/myfirstapplication/MyFirstApplicationTests.java
+++ MyFirstApplication/src/test/java/com/example/myfirstapplication/MyFirstApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.spring;
+package com.example.myfirstapplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class MyFirstApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
